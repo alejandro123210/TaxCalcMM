f02ceedccf18bafd14a0fb9e71b1993e13ca95cd
Alejandro Gonzalez <myname123210@gmail.com>
2015-07-07 23:58:59 -0400

    Initial Commit

---
+++ TaxCalc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TaxCalc
+//
+//  Created by Alejandro Gonzalez on 7/7/15.
+//  Copyright (c) 2015 Alejandro Gonzalez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
